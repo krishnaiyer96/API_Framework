5ed53c27ba5c3ad34b52b137178878a125b3ca5f
krishnaiyer96 <krishna.iyer17@gmail.com>
2024-11-12 08:14:28 +0530

    commiting conftest update

--- conftest.py
+++ conftest.py
@@ -7,15 +7,17 @@ def pytest_addoption(parser):
     parser.addoption("--host", action= "store", default= "uat")
 
 def update_env(config):
-    with open("D:\\Source\\Python\\Automation\\Framework\\BackendFramework\\API_Framework\\config\\endpoints.json") as end_point:
+    with open("D:\\Source\\Python\\Automation\\Framework\\BackendFramework\\API_Framework\\config\\endpoints.json", 'w+') as end_point:
         data = json.load(end_point)
-        end_point.truncate(0)
+        print(data)
         end_point.seek(0)
+        end_point.truncate(0)
+
         data['environment']['env'] = config.getoption("--host").lower()
         json.dump(data, end_point, indent=4)
         end_point.close()
 
-@pytest.hookimpl
+@pytest.hookimpl()
 def pytest_configure(config):
     update_env(config)
 
