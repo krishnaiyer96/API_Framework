5f623b8272df9918f6a81c06293432a08858cd14
Krishna.Iyer <krishna.iyer@stonex.com>
2024-11-12 14:43:35 +0530

    Pushing the Conftest file changes

--- conftest.py
+++ conftest.py
@@ -4,23 +4,21 @@ import pytest
 
 
 def pytest_addoption(parser):
-    parser.addoption("--host", action= "store", default= "uat")
+    parser.addoption("--host", action="store", default="uat")
+
 
 def update_env(config):
-    with open("D:\\Source\\Python\\Automation\\Framework\\BackendFramework\\API_Framework\\config\\endpoints.json", 'w+') as end_point:
+    with open("C:\\Users\\Krishna.Iyer\\Source\\Automation\\Python\\API_Framework\\config\\endpoints.json",
+              'r+') as end_point:
         data = json.load(end_point)
-        print(data)
-        end_point.seek(0)
-        end_point.truncate(0)
 
+        end_point.truncate(0)
+        end_point.seek(0)
         data['environment']['env'] = config.getoption("--host").lower()
         json.dump(data, end_point, indent=4)
         end_point.close()
 
+
 @pytest.hookimpl()
 def pytest_configure(config):
     update_env(config)
-
-
-
-
